6b6b3e5e6746f5c11b99a4606a4aabe0f3a7485c
Johnny Fung <46305146+Johnny-Fung@users.noreply.github.com>
2019-12-28 02:54:55 -0500

    Added chart

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Map Portion
 
 
 // Instantiation of map object given instance of HTML element
@@ -129,3 +130,64 @@ legend.onAdd = function (map) {
 };
 // Display the legend on map area
 legend.addTo(map);
+
+
+
+// Chart Area:
+
+var ctx = document.getElementById('graph').getContext('2d');
+function renderChart(data, labels) {
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    })
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
